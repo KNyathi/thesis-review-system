8d4f0698c50c38fb7b8b946edd4730aca3f75e5a
KNyathi <khayelihlenyathi07@gmail.com>
2025-06-17 18:11:10 +0300

    added .env file in prep for prod

--- client/src/services/api.js
+++ client/src/services/api.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const API_BASE_URL = "http://localhost:8000/api/v1";
+const API_BASE_URL = process.env.REACT_APP_API_BASE_URL;
 
 // Axios instance with default config
 const api = axios.create({
@@ -65,7 +65,7 @@ const realThesisAPI = {
     });
     return response.data;
   },
-  
+
   // Admin endpoints
   getAllUsers: async () => {
     const response = await api.get("/users");
