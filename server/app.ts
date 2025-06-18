751b76bf71333f62eb69e718b1f9cefe78183ffc
KNyathi <khayelihlenyathi07@gmail.com>
2025-06-18 19:28:02 +0300

    checking errors

--- server/app.ts
+++ server/app.ts
@@ -14,6 +14,7 @@ import path from 'path';
 
 //Body parser
 app.use(express.json({ limit: "50mb" }));
+console.log("Express router path:", require.resolve("express/lib/router"));
 
 //cookie parse
 app.use(cookieParser());
